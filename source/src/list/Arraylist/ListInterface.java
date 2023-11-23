fac40051551d8c331772f76eee38dada67996fd8
hyunjin_byun <shinbm219@naver.com>
2023-11-23 18:40:51 +0900

    배열리스트ì 인터페이스(제네릭)

---
+++ source/src/list/Arraylist/ListInterface.java
@@ -0,0 +1,14 @@
+package list.Arraylist;
+
+public interface ListInterface<E> {
+    public void add(int i , E x);
+    public void append(E x);
+    public E remove(int i);
+    public boolean removeItem(E X);
+    public E get(int i);
+    public void set(int i,E x);
+    public int indexOf(E x);
+    public int len();
+    public boolean isEmpty();
+    public void clear();
+}
