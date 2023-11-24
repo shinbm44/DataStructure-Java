e3290b91e177086244aae8a8dcfe378b2ebc87e6
hyunjin_byun <shinbm219@naver.com>
2023-11-24 10:53:36 +0900

    연결리스트 노드

---
+++ source/src/list/LinkedList/Node.java
@@ -0,0 +1,15 @@
+package list.LinkedList;
+
+public class Node<E> {
+    public E item;
+    public Node<E> next;
+    public Node(E newitem){
+        item = newitem;
+        next = null;
+    }
+
+    public Node(E newitem, Node<E> NextNode){
+        item = newitem;
+        next = NextNode;
+    }
+}
