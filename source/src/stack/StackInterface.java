848efa8d7ce0cfc553266af0f2e227ad18c2f08a
hyunjin_byun <shinbm219@naver.com>
2023-12-13 17:27:39 +0900

    스택 인터페이스

---
+++ source/src/stack/StackInterface.java
@@ -0,0 +1,10 @@
+package stack;
+
+public interface StackInterface<E> {
+    public void push(E newItem);
+    public E pop();
+    public E top();
+    public boolean isEmpty();
+    public void popAll();
+
+}
