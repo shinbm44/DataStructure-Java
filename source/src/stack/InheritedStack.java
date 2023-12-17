706f7b2e8dfb700ca208e535e45fcc75e930f360
hyunjin_byun <shinbm219@naver.com>
2023-12-17 19:48:59 +0900

    연결리스트 상속 스택 구현

---
+++ source/src/stack/InheritedStack.java
@@ -0,0 +1,33 @@
+package stack;
+
+import list.LinkedList.LinkedList;
+
+public class InheritedStack<E> extends LinkedList<E> implements StackInterface<E> {
+
+    public InheritedStack() {
+        super();
+    }
+
+    @Override
+    public void push(E newItem) {
+        add(0, newItem);
+    }
+
+    @Override
+    public E pop() {
+        if(!isEmpty()){
+            return remove(0);
+        }
+        return null;
+    }
+
+    @Override
+    public E top() {
+        return get(0);
+    }
+
+    @Override
+    public void popAll() {
+        clear();
+    }
+}
