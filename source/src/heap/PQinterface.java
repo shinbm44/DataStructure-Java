a61308de17ffe2d1b38346290e90ac63ba3035e4
hyunjin_byun <shinbm219@naver.com>
2023-12-24 23:47:45 +0900

    우선순위 큐 인터페이스

---
+++ source/src/heap/PQinterface.java
@@ -0,0 +1,9 @@
+package heap;
+
+public interface PQinterface <E>{
+    public void insert(E newItem) throws Exception;
+    public E deleteMax() throws Exception;
+    public E max() throws Exception;
+    public boolean isEmpty();
+    public void clear();
+}
